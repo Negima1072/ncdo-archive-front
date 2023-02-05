6e1bfc9ae7414d3700fd14c9ecfd9cacc430177b
Negima1072 <tkser@icloud.com>
2023-02-05 21:48:27 +0900

    updt: iroiro

--- src/pages/VideoListPage/VideoListPage.tsx
+++ src/pages/VideoListPage/VideoListPage.tsx
@@ -1,19 +1,7 @@
-import {
-  ColumnDef,
-  flexRender,
-  getCoreRowModel,
-  getPaginationRowModel,
-  useReactTable,
-} from "@tanstack/react-table";
-import React, { useEffect, useMemo, useState } from "react";
-import {
-  Button,
-  ButtonGroup,
-  Container,
-  Form,
-  InputGroup,
-  Table,
-} from "react-bootstrap";
+import { ColumnDef } from "@tanstack/react-table";
+import usePageTable from "components/usePageTable/usePageTable";
+import React, { useMemo } from "react";
+import { Button, Container, Form, InputGroup } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import Styled from "./VideoListPage.module.scss";
 
@@ -46,25 +34,19 @@ const COLUMNS: ColumnDef<VideoData>[] = [
 
 const VideoListPage = () => {
   const columns = useMemo(() => COLUMNS, []);
-  const [videos, setVideos] = useState<VideoData[]>([]);
-  const [totalSize, setTotalSize] = useState<number>(0);
-  const table = useReactTable<VideoData>({
-    data: videos,
+  const [Table, totalSize] = usePageTable<VideoData>({
     columns: columns,
-    getCoreRowModel: getCoreRowModel(),
-    getPaginationRowModel: getPaginationRowModel(),
-  });
-  useEffect(() => {
-    (async () => {
+    pageSize: 10,
+    page: 1,
+    getPage: async (page, pageSize) => {
       const res = await fetch(
-        `https://api.ncdo.net/v1/archive/list?status=3&pageSize=10&page=1`
+        `https://api.ncdo.net/v1/archive/list?status=3&pageSize=${pageSize}&page=${page}`
       );
       if (res.status === 200) {
         const resJson = await res.json();
         if (resJson.meta.status === 200) {
-          setTotalSize(resJson.data.totalCount);
-          setVideos(
-            resJson.data.items.map((v: ListItem): VideoData => {
+          return {
+            items: resJson.data.items.map((v: ListItem): VideoData => {
               return {
                 id: v.id,
                 title: v.videoTitle ?? v.videoId,
@@ -72,12 +54,15 @@ const VideoListPage = () => {
                 commentNum: v.count.nowComment,
                 updatedAt: v.updatedAt ?? "",
               };
-            })
-          );
+            }),
+            totalCount: resJson.data.totalCount,
+            hasNext: resJson.data.hasNext,
+          };
         }
       }
-    })();
-  }, []);
+      return { items: [], totalCount: 0, hasNext: false };
+    },
+  });
   return (
     <>
       <h2>動画一覧</h2>
@@ -90,86 +75,7 @@ const VideoListPage = () => {
           </Button>
         </InputGroup>
       </Container>
-      <Container>
-        <Table striped bordered className={Styled.videoTable}>
-          <thead>
-            {table.getHeaderGroups().map((headerGroup) => (
-              <tr key={headerGroup.id}>
-                {headerGroup.headers.map((header) => (
-                  <th key={header.id} colSpan={header.colSpan}>
-                    {header.isPlaceholder
-                      ? null
-                      : flexRender(
-                          header.column.columnDef.header,
-                          header.getContext()
-                        )}
-                  </th>
-                ))}
-              </tr>
-            ))}
-          </thead>
-          <tbody>
-            {table.getRowModel().rows.map((row) => {
-              return (
-                <tr key={row.id}>
-                  {row.getVisibleCells().map((cell) => {
-                    return (
-                      <td key={cell.id}>
-                        {flexRender(
-                          cell.column.columnDef.cell,
-                          cell.getContext()
-                        )}
-                      </td>
-                    );
-                  })}
-                </tr>
-              );
-            })}
-          </tbody>
-        </Table>
-      </Container>
-      <Container className={Styled.pageButton}>
-        <ButtonGroup>
-          <Button
-            variant="dark"
-            onClick={() => table.setPageIndex(0)}
-            disabled={!table.getCanPreviousPage()}
-          >
-            {"<<"}
-          </Button>
-          <Button
-            variant="dark"
-            onClick={() => table.previousPage()}
-            disabled={!table.getCanPreviousPage()}
-          >
-            {"<"}
-          </Button>
-        </ButtonGroup>
-        <div style={{ margin: "5px" }}>
-          <span>Page</span>
-          <strong>
-            {table.getState().pagination.pageIndex + 1}
-            {" of "}
-            {table.getPageCount()}
-          </strong>
-        </div>
-        <ButtonGroup>
-          <Button
-            variant="dark"
-            onClick={() => table.nextPage()}
-            disabled={!table.getCanNextPage()}
-          >
-            {">"}
-          </Button>
-          <Button
-            variant="dark"
-            onClick={() => table.setPageIndex(table.getPageCount() - 1)}
-            disabled={!table.getCanNextPage()}
-          >
-            {">>"}
-          </Button>
-        </ButtonGroup>
-      </Container>
+      {Table}
     </>
   );
 };
